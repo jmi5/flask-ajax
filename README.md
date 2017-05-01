ffd8512da24b08aee8992e389d471fc6d1736acd
Josh Izzard <jizzard@redventures.com>
2017-05-01 04:43:39 -0400

    working checkpoint

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # flask-ajax
-Working on getting this http://codehandbook.org/python-flask-jquery-ajax-post/ to work
+Working on getting this http://codehandbook.org/python-flask-jquery-ajax-post/ to work.
+
+### Notes
+- At the beginning of the project, run `virtualenv env`.
+	+ This creates the virtual environment that you will ship with your project. 
+- After installing the necessary packages run `pip freeze > requirements.txt`
+- Does there need to be a `setup.py` script? 
+- Heroku only officially supports two runtimes: https://devcenter.heroku.com/articles/python-runtimes
+- Need a Procfile
+
+
---
+++ app.py
@@ -0,0 +1,41 @@
+#!flask-ajax/bin/python
+from flask import Flask, jsonify, request, session
+# from multiprocessing import Value
+app = Flask(__name__)
+app.secret_key = 'joshsupersecret'
+
+def safe_sess_json(session):
+    finaldict = {}
+    for thekey in ['shopInternet', 'filo', 'myAccount']:
+        if thekey in session:
+            finaldict[thekey] = session[thekey]
+        else:
+            finaldict[thekey] = 0
+    return jsonify(finaldict)
+
+@app.route('/', methods=['GET'])
+def get_all_things():
+        try:
+            out = jsonify(shopInternet = session['shopInternet'],
+                   filo = session['filo'],
+                   myAccount = session['myAccount'])
+        except KeyError:
+            out = jsonify(message = 'No data yet')
+        return out
+
+
+@app.route('/event', methods=['POST'])
+def increment_count():
+    content = request.get_json()
+    if 'eventName' in content:
+        if not content['eventName'] in session:
+            session[content['eventName']] = 1
+        else:
+            session[content['eventName']] += 1
+    return safe_sess_json(session=session)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
